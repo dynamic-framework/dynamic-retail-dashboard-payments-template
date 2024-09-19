87226e2df11f2ed84ebf5d81045778ebd921cf8c
Johann Felipe Rojas <johannrip@gmail.com>
2024-09-19 15:26:13 -0500

    chore: replace general skeleton with single loader component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import CategoryList from './components/CategoryList';
+import CategoryLoader from './components/loaders/CategoryLoader';
 import ScheduledPayments from './components/ScheduledPayments';
-import SkeletonLoader from './components/SkeletonLoader';
 import useLoanAccountsCategorized from './services/hooks/useLoanAccountsCategorized';
 
 export default function App() {
   const { loading } = useLoanAccountsCategorized();
   return (
     <div className="py-4">
-      {loading
-        ? <SkeletonLoader />
-        : (
-          <div className="row">
-            <div className="col-12 col-lg-7 col-xl-8 pb-6 order-2">
-              <CategoryList />
-            </div>
-            <div className="col-12 col-lg-5 col-xl-4 order-3">
-              <ScheduledPayments />
-            </div>
-          </div>
-        )}
+      <div className="row">
+        <div className="col-12 col-lg-7 col-xl-8 pb-6 order-2">
+          {loading
+            ? <CategoryLoader />
+            : <CategoryList />}
+        </div>
+        <div className="col-12 col-lg-5 col-xl-4 order-3">
+          <ScheduledPayments />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/loaders/ScheduledLoader.tsx
@@ -0,0 +1,12 @@
+export default function ScheduledLoader() {
+  return (
+    <div className="d-flex flex-column gap-6 placeholder-glow">
+      <span
+        className="placeholder bg-secondary-200 rounded-1"
+        style={{
+          height: '24rem',
+        }}
+      />
+    </div>
+  );
+}
