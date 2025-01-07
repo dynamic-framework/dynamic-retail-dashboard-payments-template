d2eaf022875e94643d0e0e3dc8f1ce385c52735a
Johann F Rojas <43022731+johannrp27@users.noreply.github.com>
2025-01-07 14:23:31 -0500

    chore: replace general skeleton with single loader component (#15)
    Co-authored-by: lnazarit <lnazarit@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import CategoryList from './components/CategoryList';
 import DashboardFeatures from './components/DashboardFeatures';
-import Loading from './components/Loading';
+import CategoryLoader from './components/loaders/CategoryLoader';
 import useLoanAccountsCategorized from './services/hooks/useLoanAccountsCategorized';
-import useLocalData from './services/hooks/useLocalData';
 
 export default function App() {
   const { loading } = useLoanAccountsCategorized();
-  useLocalData();
-
-  if (loading) {
-    return <Loading />;
-  }
-
   return (
-    <div className="py-4">
-      <div className="row position-relative align-items-start gx-0 gx-md-6">
-        <div className="col-12 col-lg-7 col-xl-8 pb-6 order-2">
-          <CategoryList />
-        </div>
-        <div className="col-12 col-lg-5 col-xl-4 order-3 position-sticky sticky-md-top">
-          <DashboardFeatures />
+    <div className="container">
+      <div className="py-4">
+        <div className="row position-relative align-items-start gx-0 gx-md-6">
+          <div className="col-12 col-lg-7 col-xl-8 pb-6 order-2">
+            {loading
+              ? <CategoryLoader />
+              : <CategoryList />}
+          </div>
+          <div className="col-12 col-lg-5 col-xl-4 order-3 position-sticky sticky-md-top">
+            <DashboardFeatures />
+          </div>
         </div>
       </div>
     </div>
